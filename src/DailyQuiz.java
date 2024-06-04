28562b4f82995dd3fcf341d8d7ceeee4377ced86
jo junho <junhosvain@naver.com>
2024-06-04 21:40:36 +0900

    :ghost: 자바(06/04 - DailyQuiz)

---
+++ src/DailyQuiz.java
@@ -0,0 +1,23 @@
+public class DailyQuiz {
+    public static void main(String[] args) {
+        // 6월 4일
+        // 1번
+         /*int count = 1;
+         while (true){
+             count++;
+             if (count == 10001){
+                 break;
+             }
+             System.out.println(count);
+         }*/
+
+        // 2번
+        int mul;
+        for (int i = 9; i > 1; i--) {
+            for (int j = 1; j < 10; j++) {
+                mul = i * j;
+                System.out.println(i+"*"+j+"="+mul);
+            }
+        }
+    }
+}
